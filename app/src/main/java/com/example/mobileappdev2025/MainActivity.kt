33053a4de3df1b2b5be1caa74747c2996454f31c
Joshua Deaton <joshwdeaton@gmail.com>
2025-04-18 09:58:46 -0500

    New final repo

--- app/src/main/java/com/example/mobileappdev2025/MainActivity.kt
+++ app/src/main/java/com/example/mobileappdev2025/MainActivity.kt
@@ -9,6 +9,7 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.ListView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -18,124 +19,47 @@ import java.io.FileInputStream
 import java.util.Random
 import java.util.Scanner
 
-data class WordDefinition(val word: String, val definition: String);
+class MainActivity : AppCompatActivity()
+{ // Write code for group chats. Agree on an application name. Meet up to work on it together.
+    private lateinit var firebaseAuth : FireBaseaUTH
+    private lateinit var databaase : FirebaseFireStore
 
-class MainActivity : AppCompatActivity() {
-    private val ADD_WORD_CODE = 1234; // 1-65535
-    private lateinit var myAdapter : ArrayAdapter<String>; // connect from data to gui
-    private var dataDefList = ArrayList<String>(); // data
-    private var wordDefinition = mutableListOf<WordDefinition>();
-    private var score : Int = 1;
-    private var totalCorrect : Int = 2;
-    private var totalWrong : Int = 3;
-
-    override fun onCreate(savedInstanceState: Bundle?) {
+    }
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main))
+        { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        loadWordsFromDisk()
-
-        pickNewWordAndLoadDataList();
-        setupList();
-
-        val defList = findViewById<ListView>(R.id.dynamic_def_list);
-        defList.setOnItemClickListener { _, _, index, _ ->
-            pickNewWordAndLoadDataList();
-            myAdapter.notifyDataSetChanged();
-        };
+        // val user = firebaseAuth.CurrentUser
     }
 
+    var hardcodeJson = "{\"Eric\": { \"age\": 28}}"
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
+    var jo = JSONObject(hardcodeJson)
+    var eric = jo.getJSONObject(name:"Eric")
 
-        if (requestCode == ADD_WORD_CODE && resultCode == RESULT_OK && data != null){
-            val word = data.getStringExtra("word")?:""
-            val def = data.getStringExtra("def")?:""
+    if (eric.has(name:"age"))
+        var age = eric.getInt(name:"age") ?: -1
 
-            Log.d("MAD", word)
-            Log.d("MAD", def)
-
-            if ( word == "" || def == "")
-                return
-
-            wordDefinition.add(WordDefinition(word, def))
-
-            pickNewWordAndLoadDataList()
-            myAdapter.notifyDataSetChanged()
-        }
-    }
-
-    private fun loadWordsFromDisk()
+    private fun getConnections(_userID : String): Task<List<Strings>
     {
-        // user data
-        val file = File(applicationContext.filesDir, "user_data.csv")
+        val connectionRef = database.collection(collectionPath:"contections").document
 
-        if (file.exists()) {
-            val readResult = FileInputStream(file)
-            val scanner = Scanner(readResult)
+        return connectionRef.get().continueWith { task ->
+            val document  = task.result
 
-            while(scanner.hasNextLine()){
-                val line = scanner.nextLine()
-                val wd = line.split("|")
-                wordDefinition.add(WordDefinition(wd[0], wd[1]))
-            }
-        } else { // default data
-
-            file.createNewFile()
-
-            val reader = Scanner(resources.openRawResource(R.raw.default_words))
-            while(reader.hasNextLine()){
-                val line = reader.nextLine()
-                val wd = line.split("|")
-                wordDefinition.add(WordDefinition(wd[0], wd[1]))
-                file.appendText("${wd[0]}|${wd[1]}\n")
+            if (document.exists())
+            {
+                val data = document.data
+                List<String> people = data?.get("people") as? List<String> ?:_emptyList()
             }
         }
-    }
-
-    private fun pickNewWordAndLoadDataList()
-    {
-        wordDefinition.shuffle();
-
-        dataDefList.clear();
-
-        for(wd in wordDefinition){
-            dataDefList.add(wd.definition);
-        }
-
-        findViewById<TextView>(R.id.word).text = wordDefinition[0].word;
-
-        dataDefList.shuffle();
-    }
-
-    private fun setupList()
-    {
-        myAdapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, dataDefList);
-
-        // connect to list
-        val defList = findViewById<ListView>(R.id.dynamic_def_list);
-        defList.adapter = myAdapter;
-    }
-
-    fun openStats(view : View)
-    {
-        var myIntent = Intent(this, StatsActivity::class.java);
-        myIntent.putExtra("score", score.toString());
-        myIntent.putExtra("totalCorrect", totalCorrect.toString());
-        myIntent.putExtra("totalWrong", totalWrong.toString());
-        startActivity(myIntent)
-    }
-
-    fun openAddWord(view : View)
-    {
-        var myIntent = Intent(this, AddWordActivity::class.java);
-        startActivityForResult(myIntent, ADD_WORD_CODE)
     }
 }
